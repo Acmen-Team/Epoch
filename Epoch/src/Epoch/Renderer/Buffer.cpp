397c7568ccde1ea770951793c680cdaf15103e92
YxY <876285983@qq.com>
2021-03-03 22:34:45 +0800

    feat: add Buffer class

---
+++ Epoch/src/Epoch/Renderer/Buffer.cpp
@@ -0,0 +1,19 @@
+#include "eppch.h"
+
+#include "Buffer.h"
+
+#include "Platform/OpenGL/OpenGLBuffer.h"
+
+namespace Epoch {
+
+  VertexBuffer* VertexBuffer::Create(float* vertices, uint32_t size)
+  {
+	return new OpenGLVertexBuffer(vertices, size);
+  }
+
+  IndexBuffer* IndexBuffer::Create(uint32_t* indices, uint32_t count)
+  {
+	return new OpenGLIndexBuffer(indices, count);
+  }
+
+}
