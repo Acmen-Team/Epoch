95d3408f15333b3307a2efec6511699ab3c4a64e
YxY <876285983@qq.com>
2021-03-03 22:33:10 +0800

    feat: add ImGui Layer

---
+++ Epoch/src/Epoch/ImGui/ImGuiLayer.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Epoch/Layer.h"
+
+#include "Epoch/Events/ApplicationEvent.h"
+#include "Epoch/Events/KeyEvent.h"
+#include "Epoch/Events/MouseEvent.h"
+
+namespace Epoch {
+
+  class ImGuiLayer : public Layer
+  {
+  public:
+	ImGuiLayer();
+	~ImGuiLayer();
+
+	virtual void OnAttach() override;
+	virtual void OnDetach() override;
+	virtual void OnImGuiRender() override;
+
+	void Begin();
+	void End();
+  private:
+	float m_Time = 0.0f;
+  };
+
+}
