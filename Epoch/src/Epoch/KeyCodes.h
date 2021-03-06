5e878e3e6c91f2a83a754e7029b1cca5ab37ecd0
YxY <876285983@qq.com>
2021-03-06 21:19:43 +0800

    faet: add Input system

---
+++ Epoch/src/Epoch/KeyCodes.h
@@ -0,0 +1,128 @@
+#pragma once
+
+// Copy from glfw3.h
+#define EP_KEY_UNKNOWN            -1
+
+/* Printable keys */
+#define EP_KEY_SPACE              32
+#define EP_KEY_APOSTROPHE         39  /* ' */
+#define EP_KEY_COMMA              44  /* , */
+#define EP_KEY_MINUS              45  /* - */
+#define EP_KEY_PERIOD             46  /* . */
+#define EP_KEY_SLASH              47  /* / */
+#define EP_KEY_0                  48
+#define EP_KEY_1                  49
+#define EP_KEY_2                  50
+#define EP_KEY_3                  51
+#define EP_KEY_4                  52
+#define EP_KEY_5                  53
+#define EP_KEY_6                  54
+#define EP_KEY_7                  55
+#define EP_KEY_8                  56
+#define EP_KEY_9                  57
+#define EP_KEY_SEMICOLON          59  /* ; */
+#define EP_KEY_EQUAL              61  /* = */
+#define EP_KEY_A                  65
+#define EP_KEY_B                  66
+#define EP_KEY_C                  67
+#define EP_KEY_D                  68
+#define EP_KEY_E                  69
+#define EP_KEY_F                  70
+#define EP_KEY_G                  71
+#define EP_KEY_H                  72
+#define EP_KEY_I                  73
+#define EP_KEY_J                  74
+#define EP_KEY_K                  75
+#define EP_KEY_L                  76
+#define EP_KEY_M                  77
+#define EP_KEY_N                  78
+#define EP_KEY_O                  79
+#define EP_KEY_P                  80
+#define EP_KEY_Q                  81
+#define EP_KEY_R                  82
+#define EP_KEY_S                  83
+#define EP_KEY_T                  84
+#define EP_KEY_U                  85
+#define EP_KEY_V                  86
+#define EP_KEY_W                  87
+#define EP_KEY_X                  88
+#define EP_KEY_Y                  89
+#define EP_KEY_Z                  90
+#define EP_KEY_LEFT_BRACKET       91  /* [ */
+#define EP_KEY_BACKSLASH          92  /* \ */
+#define EP_KEY_RIGHT_BRACKET      93  /* ] */
+#define EP_KEY_GRAVE_ACCENT       96  /* ` */
+#define EP_KEY_WORLD_1            161 /* non-US #1 */
+#define EP_KEY_WORLD_2            162 /* non-US #2 */
+
+/* Function keys */
+#define EP_KEY_ESCAPE             256
+#define EP_KEY_ENTER              257
+#define EP_KEY_TAB                258
+#define EP_KEY_BACKSPACE          259
+#define EP_KEY_INSERT             260
+#define EP_KEY_DELETE             261
+#define EP_KEY_RIGHT              262
+#define EP_KEY_LEFT               263
+#define EP_KEY_DOWN               264
+#define EP_KEY_UP                 265
+#define EP_KEY_PAGE_UP            266
+#define EP_KEY_PAGE_DOWN          267
+#define EP_KEY_HOME               268
+#define EP_KEY_END                269
+#define EP_KEY_CAPS_LOCK          280
+#define EP_KEY_SCROLL_LOCK        281
+#define EP_KEY_NUM_LOCK           282
+#define EP_KEY_PRINT_SCREEN       283
+#define EP_KEY_PAUSE              284
+#define EP_KEY_F1                 290
+#define EP_KEY_F2                 291
+#define EP_KEY_F3                 292
+#define EP_KEY_F4                 293
+#define EP_KEY_F5                 294
+#define EP_KEY_F6                 295
+#define EP_KEY_F7                 296
+#define EP_KEY_F8                 297
+#define EP_KEY_F9                 298
+#define EP_KEY_F10                299
+#define EP_KEY_F11                300
+#define EP_KEY_F12                301
+#define EP_KEY_F13                302
+#define EP_KEY_F14                303
+#define EP_KEY_F15                304
+#define EP_KEY_F16                305
+#define EP_KEY_F17                306
+#define EP_KEY_F18                307
+#define EP_KEY_F19                308
+#define EP_KEY_F20                309
+#define EP_KEY_F21                310
+#define EP_KEY_F22                311
+#define EP_KEY_F23                312
+#define EP_KEY_F24                313
+#define EP_KEY_F25                314
+#define EP_KEY_KP_0               320
+#define EP_KEY_KP_1               321
+#define EP_KEY_KP_2               322
+#define EP_KEY_KP_3               323
+#define EP_KEY_KP_4               324
+#define EP_KEY_KP_5               325
+#define EP_KEY_KP_6               326
+#define EP_KEY_KP_7               327
+#define EP_KEY_KP_8               328
+#define EP_KEY_KP_9               329
+#define EP_KEY_KP_DECIMAL         330
+#define EP_KEY_KP_DIVIDE          331
+#define EP_KEY_KP_MULTIPLY        332
+#define EP_KEY_KP_SUBTRACT        333
+#define EP_KEY_KP_ADD             334
+#define EP_KEY_KP_ENTER           335
+#define EP_KEY_KP_EQUAL           336
+#define EP_KEY_LEFT_SHIFT         340
+#define EP_KEY_LEFT_CONTROL       341
+#define EP_KEY_LEFT_ALT           342
+#define EP_KEY_LEFT_SUPER         343
+#define EP_KEY_RIGHT_SHIFT        344
+#define EP_KEY_RIGHT_CONTROL      345
+#define EP_KEY_RIGHT_ALT          346
+#define EP_KEY_RIGHT_SUPER        347
+#define EP_KEY_MENU               348
---
+++ Epoch/src/Epoch/MouseButtonCodes.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// Copy from glfw3.h
+#define EP_MOUSE_BUTTON_1         0
+#define EP_MOUSE_BUTTON_2         1
+#define EP_MOUSE_BUTTON_3         2
+#define EP_MOUSE_BUTTON_4         3
+#define EP_MOUSE_BUTTON_5         4
+#define EP_MOUSE_BUTTON_6         5
+#define EP_MOUSE_BUTTON_7         6
+#define EP_MOUSE_BUTTON_8         7
+#define EP_MOUSE_BUTTON_LAST      EP_MOUSE_BUTTON_8
+#define EP_MOUSE_BUTTON_LEFT      EP_MOUSE_BUTTON_1
+#define EP_MOUSE_BUTTON_RIGHT     EP_MOUSE_BUTTON_2
+#define EP_MOUSE_BUTTON_MIDDLE    EP_MOUSE_BUTTON_3
