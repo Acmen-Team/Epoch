ea426386b2ee7e3de84a8368c09858e177027f3c
YxY <876285983@qq.com>
2021-04-04 10:28:53 +0800

    feat: change camera rotation vector

--- Epoch/src/Epoch/Renderer/PrespectiveCamera.h
+++ Epoch/src/Epoch/Renderer/PrespectiveCamera.h
@@ -11,8 +11,8 @@ namespace Epoch {
 
 	const glm::vec3& GetPosition() const { return m_Position; }
 	void SetPosition(const glm::vec3& position) { m_Position = position; RecalculateViewMatrix(); }
-	float GetRotation() const { return m_Rotation; }
-	void SetRotation(float rotation) { m_Rotation = rotation; RecalculateViewMatrix(); }
+	glm::vec3 GetRotation() const { return m_Rotation; }
+	void SetRotation(const glm::vec3& rotation) { m_Rotation = rotation; RecalculateViewMatrix(); }
 
 	const glm::mat4& GetProjectionMatrix() const { return m_ProjectionMatrix; }
 	const glm::mat4& GetViewMatrix() const { return m_ViewMatrix; }
@@ -25,7 +25,7 @@ namespace Epoch {
 	glm::mat4 m_ViewProjectionMatrix;
 
 	glm::vec3 m_Position = { 0.0f, 0.0f, 0.0f };
-	float m_Rotation = 0.0f;
+	glm::vec3 m_Rotation = { 0.0f, 0.0f, 0.0f };
   };
 
 }
