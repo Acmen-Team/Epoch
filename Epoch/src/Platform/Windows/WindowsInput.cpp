e8dd9e2b4e2c2cbeabda530e536fa072b3cac778
YxY <876285983@qq.com>
2021-05-24 12:59:24 +0800

    refactor: Decoupling the Input module to improve concentration

--- Epoch/src/Platform/Windows/WindowsInput.cpp
+++ Epoch/src/Platform/Windows/WindowsInput.cpp
@@ -1,15 +1,13 @@
 #include "eppch.h"
 
-#include "WindowsInput.h"
+#include "Epoch/Core/Input.h"
 #include "Epoch/Core/Application.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Epoch {
 
-  Input* Input::s_Instance = new WindowsInput();
-
-  bool WindowsInput::IsKeyPressedImpl(int keycode)
+  bool Input::IsKeyPressed(int keycode)
   {
 	auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 	auto state = glfwGetKey(window, keycode);
@@ -17,7 +15,7 @@ namespace Epoch {
 	return state == GLFW_PRESS || state == GLFW_REPEAT;
   }
 
-  bool WindowsInput::IsMouseButtonPressedImpl(int button)
+  bool Input::IsMouseButtonPressed(int button)
   {
 	auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 	auto state = glfwGetMouseButton(window, button);
@@ -25,7 +23,7 @@ namespace Epoch {
 	return state == GLFW_PRESS;
   }
 
-  std::pair<float, float> WindowsInput::GetMousePositionImpl()
+  std::pair<float, float> Input::GetMousePosition()
   {
 	auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 	double xpos, ypos;
@@ -34,16 +32,16 @@ namespace Epoch {
 	return { (float)xpos, (float)ypos };
   }
 
-  float WindowsInput::GetMouseXImpl()
+  float Input::GetMouseX()
   {
-	auto[x, y] = GetMousePositionImpl();
+	auto[x, y] = GetMousePosition();
 
 	return x;
   }
 
-  float WindowsInput::GetMouseYImpl()
+  float Input::GetMouseY()
   {
-	auto[x, y] = GetMousePositionImpl();
+	auto[x, y] = GetMousePosition();
 
 	return y;
   }
