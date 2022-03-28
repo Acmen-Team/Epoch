323d0a01fd2adb3a51818f84cf7dc56a57aea6c9
YxY <876285983@qq.com>
2022-03-28 22:47:13 +0800

    feat: add scene serialize framework(implementation: SiYu)

---
+++ Epoch/src/Epoch/Scene/SceneSerialize.cpp
@@ -0,0 +1,13 @@
+#include "eppch.h"
+
+#include "SceneSerialize.h"
+
+namespace Epoch {
+
+  SceneSerialize::SceneSerialize(const Scene& scene)
+	: m_Scene(scene)
+  {
+
+  }
+
+}
---
+++ Epoch/src/Epoch/Scene/SceneSerialize.h
@@ -0,0 +1,50 @@
+﻿/*************************************************
+Copyright © 2020 - 2022 YX.All Rights Reserved
+
+Date:2022-3-28 22:02
+Description:Serialization
+**************************************************/
+#pragma once
+
+#include "Epoch/Scene/Scene.h"
+
+// TODO : SiYu
+
+namespace Epoch {
+
+  class SceneSerialize
+  {
+  public:
+	SceneSerialize(const Scene& scene);
+
+	/*
+	* Serialize a scene.
+	*/
+	bool Serialize(const std::string& filePath);
+	bool DeSerialize(const std::string& filePath);
+
+	/*
+	* To sequence at runtime, may be used to preserve the dynamic content of a genetically continuous scene.
+	* If the timestep is equal to 16ms, ideally the number of scenes we save should be 60.
+	*/
+	bool SerializeRuntime(const std::string& filePath, unsigned int timeStep);
+	bool DeSerializeRuntime(const std::string& filePath);
+  private:
+	// Do we need these?
+	
+	/*
+	* Serialized into binary files for easier storage or transmission.
+	*/
+	// T SerializeBinary(...);
+	
+	/*
+	* EM (Tentative:EpochMap) is a map file in engine format. 
+	* Reserved for possible future existence and intended to be shared among other engines. 
+	* For example: pass a map in Epoch and then import it into Dark for use.
+	*/
+	// T SerializeEM(...);
+  private:
+	const Scene& m_Scene;
+  };
+
+}
